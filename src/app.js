dbdd148e5c076d77f78e43f59c0b1c7a7463d47f
NikitaMetelev <Kyle.B@mail.ru>
2025-06-30 09:30:52 +0500

    first build 1.0

---
+++ src/app.js
@@ -0,0 +1,131 @@
+const express = require('express')
+const http = require('http')
+const path = require('path')
+const cors = require('cors')
+const dotnev = require('dotenv')
+const fetch = require('node-fetch')
+
+
+// app
+
+const app = express()
+
+// use
+
+const pathRes = path.join(__dirname, '../public')
+console.log(pathRes)
+
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+
+// 
+
+dotnev.config()
+
+// 
+
+
+
+
+
+
+const getCurs = async () => {
+    try {
+        const responce = await fetch(`https://www.cbr-xml-daily.ru/daily_json.js`, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            }
+        })
+
+        if (!responce.ok) {
+            throw new Error(`Error: ${responce.status}`)
+        }
+
+        const data = await responce.json()
+        return data
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const getWeather = async () => {
+    try {
+        const responce = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Ufa&units=metric&appid=${process.env.TOKEN}`, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            }
+        })
+
+        const data = await responce.json()
+        console.log(data)
+        return data
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+
+const result = {}
+
+const getData = async () => {
+
+    const timeHour = new Date().getHours()
+    const timeMin = new Date().getMinutes()
+    const date = new Date().toLocaleDateString('ru', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    })
+    const day = new Date().toLocaleDateString('ru', {
+        weekday: 'long'
+    })
+
+
+    const time = `${timeHour}:${timeMin}`
+
+
+    const dataCurs = await getCurs()
+    const data = await getWeather()
+
+
+    result.date = date
+    result.day = day
+    result.time = time
+    result.img = (timeHour >= 6 && timeHour < 18) ? `${process.env.URL}/img/sun.png` : `${process.env.URL}/img/moon.png`
+    result.bg = (timeHour >= 6 && timeHour < 18) ? `${process.env.URL}/img/light.png` : `${process.env.URL}/img/dark.png`
+    result.rate = Math.floor(dataCurs.Valute.USD.Value)
+    result.weather = Math.floor(data.main.temp_max)
+
+
+}
+
+
+
+setInterval(async () => {
+    await getData();  // обновляем данные
+    console.log("Data updated:", result);  // для логирования обновлений
+}, 3600000); // 3600000 миллисекунд = 1 час
+
+
+
+
+app.get('/', async (req, res) => {
+    res.json(result)
+})
+
+
+
+getData()
+
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
