10730c43e0ff34ec55f67d2f912674ad3da4d337
Nikita Metelev <108466638+MetelevNikita@users.noreply.github.com>
2025-12-09 15:54:36 +0500

    add cash from weather and region 1.2

--- src/app.js
+++ src/app.js
@@ -350,30 +350,30 @@ app.get('/:region', async (req, res) => {
 
 
 
-app.get('/time/:region', async (req, res) => {
-    try {
+// app.get('/time/:region', async (req, res) => {
+//     try {
 
 
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
-        res.setHeader('Access-Control-Allow-Credentials', 'true');
-        res.setHeader('Access-Control-Max-Age', '86400')
+//         res.setHeader('Access-Control-Allow-Origin', '*');
+//         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
+//         res.setHeader('Access-Control-Allow-Credentials', 'true');
+//         res.setHeader('Access-Control-Max-Age', '86400')
 
-        const currentRegion = regions.data.find(region => region.name === req.params.region)
-        const info = getInfo(currentRegion.timeZone)
+//         const currentRegion = regions.data.find(region => region.name === req.params.region)
+//         const info = getInfo(currentRegion.timeZone)
 
-        res.status(200).json({
-            time: info.timeRegion,
-        })
+//         res.status(200).json({
+//             time: info.timeRegion,
+//         })
 
-    } catch (error) {
-        res.status(500).json({
-            message: `Ошибка запроса данных виджета: ${error.message}`,
-            status: 500
-        })
-    }
-})
+//     } catch (error) {
+//         res.status(500).json({
+//             message: `Ошибка запроса данных виджета: ${error.message}`,
+//             status: 500
+//         })
+//     }
+// })
 
 
 
